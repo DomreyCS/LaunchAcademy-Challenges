019e0c257d6760d1b4e966797bd5997edf6f6090
Dara Hoy <darahoy@gmail.com>
2018-11-06 12:38:14 -0500

    Pass all test for BoundingBox

--- bounding-box/lib/bounding_box.rb
+++ bounding-box/lib/bounding_box.rb
@@ -1,2 +1,32 @@
 class BoundingBox
+  attr_reader :width, :height
+
+  def initialize(x, y, width, height)
+    @x = x
+    @y = y
+    @width = width
+    @height = height
+  end
+
+  def left
+    @x
+  end
+
+  def right
+    @x + @width
+  end
+
+  def bottom
+    @y
+  end
+
+  def top
+    @y + @height
+  end
+
+  def contains_point?(x, y)
+    within_x = x >= @x && x <= @x + @width ? true : false
+    within_y = y >= @x && y <= @y + @height ? true : false
+    within_x && within_y
+  end
 end

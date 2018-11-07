b4dfdcaafe9e9d8a7440a1f96b295cf18f54a3cf
Dara Hoy <darahoy@gmail.com>
2018-11-07 14:03:43 -0500

    Write and test "Deck" class

--- blackjack/spec/lib/deck_spec.rb
+++ blackjack/spec/lib/deck_spec.rb
@@ -1,5 +1,21 @@
 require "spec_helper"
 
 RSpec.describe Deck do
-  # Your tests here
+  let(:deck) { deck = Deck.new }
+
+  describe ".new" do
+    it "should create a new deck of 52 cards" do
+      expect(deck).to be_a(Deck)
+    end
+  end
+
+  describe "#cards" do
+    it "should be an array of cards" do
+      expect(deck.cards).to be_a(Array)
+    end
+
+    it "should initialize with 52 card objects" do
+      expect(deck.cards.size).to eq(52)
+    end
+  end
 end

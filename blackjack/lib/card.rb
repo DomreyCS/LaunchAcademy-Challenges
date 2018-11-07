b6f538ead3467753046acfc69adc1bfc339d073c
Dara Hoy <darahoy@gmail.com>
2018-11-07 13:37:20 -0500

    Write Card class and test

--- blackjack/lib/card.rb
+++ blackjack/lib/card.rb
@@ -1,3 +1,19 @@
 class Card
-  # Your code here
+attr_reader :suit, :rank
+
+  def initialize(suit, rank)
+    @suit = suit
+    @rank = rank
+  end
+
+  def type?
+    type = ''
+    if ['J','Q','K'].any? { |face| @rank == face }
+      type = 'face'
+    elsif @rank == 'A'
+      type = 'ace'
+    else
+      type = 'number'
+    end
+  end
 end
--- blackjack/spec/lib/card_spec.rb
+++ blackjack/spec/lib/card_spec.rb
@@ -1,5 +1,40 @@
 require "spec_helper"
 
 RSpec.describe Card do
-  # Your tests here
+  # These UTF-8 characters will be useful for making different hands:
+  # '♦', '♣', '♠', '♥'
+
+  let(:queen_hearts) { queen_hearts = Card.new('♥', 'Q') }
+  let(:ace_spades) { ace_spades = Card.new('♠', 'A') }
+  let(:four_clubs) { four_clubs = Card.new('♣', 4) }
+
+  describe "initialize" do
+    it "should take in 2 arguments for suit and rank" do
+      expect(queen_hearts).to be_a(Card)
+    end
+  end
+
+  describe "#suit" do
+    it "should have a reader for suit" do
+      expect(queen_hearts.suit).to eq('♥')
+      expect(ace_spades.suit).to eq('♠')
+      expect(four_clubs.suit).to eq('♣')
+    end
+  end
+
+  describe "#rank" do
+    it "should have a reader for rank" do
+      expect(queen_hearts.rank).to eq('Q')
+      expect(ace_spades.rank).to eq('A')
+      expect(four_clubs.rank).to eq(4)
+    end
+  end
+
+  describe "#type?" do
+    it "should return true/false if card has a rank of 'J', 'Q' or 'K'" do
+      expect(queen_hearts.type?).to eq('face')
+      expect(four_clubs.type?).to eq('number')
+      expect(ace_spades.type?).to eq('ace')
+    end
+  end
 end
